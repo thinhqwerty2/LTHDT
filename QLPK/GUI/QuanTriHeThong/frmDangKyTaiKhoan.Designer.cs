e1d7af6dbedc3fcd77e4769079b3b9b6e84d3c7a
Anh Chi Tuan <CHI.TA194232@sis.hust.edu.vn>
2022-02-02 17:51:57 +0700

    update form QuanTriHeThong

---
+++ QLPK/GUI/QuanTriHeThong/frmDangKyTaiKhoan.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace QLPK.GUI.QuanTriHeThong
+{
+    partial class frmDangKyTaiKhoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.lblQuyenTruyCap = new System.Windows.Forms.Label();
+            this.txtMatKhauMoi = new System.Windows.Forms.TextBox();
+            this.lblXacNhanMatKhau = new System.Windows.Forms.Label();
+            this.txtMatKhauCu = new System.Windows.Forms.TextBox();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.txtHoVaTen = new System.Windows.Forms.TextBox();
+            this.lblHoVaTen = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.lblTenDangNhap = new System.Windows.Forms.Label();
+            this.lblThayDoiMatKhau = new System.Windows.Forms.Label();
+            this.cmbQuyenTruyCap = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.Location = new System.Drawing.Point(355, 358);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(109, 41);
+            this.btnDangKy.TabIndex = 15;
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            // 
+            // lblQuyenTruyCap
+            // 
+            this.lblQuyenTruyCap.AutoSize = true;
+            this.lblQuyenTruyCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuyenTruyCap.Location = new System.Drawing.Point(229, 301);
+            this.lblQuyenTruyCap.Name = "lblQuyenTruyCap";
+            this.lblQuyenTruyCap.Size = new System.Drawing.Size(111, 18);
+            this.lblQuyenTruyCap.TabIndex = 5;
+            this.lblQuyenTruyCap.Text = "Quyền truy cập:";
+            // 
+            // txtMatKhauMoi
+            // 
+            this.txtMatKhauMoi.Location = new System.Drawing.Point(367, 258);
+            this.txtMatKhauMoi.Name = "txtMatKhauMoi";
+            this.txtMatKhauMoi.Size = new System.Drawing.Size(185, 22);
+            this.txtMatKhauMoi.TabIndex = 11;
+            // 
+            // lblXacNhanMatKhau
+            // 
+            this.lblXacNhanMatKhau.AutoSize = true;
+            this.lblXacNhanMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblXacNhanMatKhau.Location = new System.Drawing.Point(229, 258);
+            this.lblXacNhanMatKhau.Name = "lblXacNhanMatKhau";
+            this.lblXacNhanMatKhau.Size = new System.Drawing.Size(139, 18);
+            this.lblXacNhanMatKhau.TabIndex = 6;
+            this.lblXacNhanMatKhau.Text = "Xác nhận mật khẩu:";
+            // 
+            // txtMatKhauCu
+            // 
+            this.txtMatKhauCu.Location = new System.Drawing.Point(367, 208);
+            this.txtMatKhauCu.Name = "txtMatKhauCu";
+            this.txtMatKhauCu.Size = new System.Drawing.Size(185, 22);
+            this.txtMatKhauCu.TabIndex = 12;
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.Location = new System.Drawing.Point(229, 208);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(73, 18);
+            this.lblMatKhau.TabIndex = 7;
+            this.lblMatKhau.Text = "Mật khẩu:";
+            // 
+            // txtHoVaTen
+            // 
+            this.txtHoVaTen.Location = new System.Drawing.Point(367, 158);
+            this.txtHoVaTen.Name = "txtHoVaTen";
+            this.txtHoVaTen.Size = new System.Drawing.Size(185, 22);
+            this.txtHoVaTen.TabIndex = 13;
+            // 
+            // lblHoVaTen
+            // 
+            this.lblHoVaTen.AutoSize = true;
+            this.lblHoVaTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoVaTen.Location = new System.Drawing.Point(229, 158);
+            this.lblHoVaTen.Name = "lblHoVaTen";
+            this.lblHoVaTen.Size = new System.Drawing.Size(75, 18);
+            this.lblHoVaTen.TabIndex = 8;
+            this.lblHoVaTen.Text = "Họ và tên:";
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(367, 109);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(185, 22);
+            this.txtTenDangNhap.TabIndex = 14;
+            // 
+            // lblTenDangNhap
+            // 
+            this.lblTenDangNhap.AutoSize = true;
+            this.lblTenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenDangNhap.Location = new System.Drawing.Point(229, 109);
+            this.lblTenDangNhap.Name = "lblTenDangNhap";
+            this.lblTenDangNhap.Size = new System.Drawing.Size(109, 18);
+            this.lblTenDangNhap.TabIndex = 9;
+            this.lblTenDangNhap.Text = "Tên đăng nhập:";
+            // 
+            // lblThayDoiMatKhau
+            // 
+            this.lblThayDoiMatKhau.AutoSize = true;
+            this.lblThayDoiMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThayDoiMatKhau.Location = new System.Drawing.Point(258, 52);
+            this.lblThayDoiMatKhau.Name = "lblThayDoiMatKhau";
+            this.lblThayDoiMatKhau.Size = new System.Drawing.Size(314, 32);
+            this.lblThayDoiMatKhau.TabIndex = 4;
+            this.lblThayDoiMatKhau.Text = "ĐĂNG KÝ TÀI KHOẢN";
+            this.lblThayDoiMatKhau.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmbQuyenTruyCap
+            // 
+            this.cmbQuyenTruyCap.FormattingEnabled = true;
+            this.cmbQuyenTruyCap.Location = new System.Drawing.Point(367, 301);
+            this.cmbQuyenTruyCap.Name = "cmbQuyenTruyCap";
+            this.cmbQuyenTruyCap.Size = new System.Drawing.Size(185, 24);
+            this.cmbQuyenTruyCap.TabIndex = 16;
+            // 
+            // frmDangKyTaiKhoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbQuyenTruyCap);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.lblQuyenTruyCap);
+            this.Controls.Add(this.txtMatKhauMoi);
+            this.Controls.Add(this.lblXacNhanMatKhau);
+            this.Controls.Add(this.txtMatKhauCu);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.txtHoVaTen);
+            this.Controls.Add(this.lblHoVaTen);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.lblTenDangNhap);
+            this.Controls.Add(this.lblThayDoiMatKhau);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmDangKyTaiKhoan";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Label lblQuyenTruyCap;
+        private System.Windows.Forms.TextBox txtMatKhauMoi;
+        private System.Windows.Forms.Label lblXacNhanMatKhau;
+        private System.Windows.Forms.TextBox txtMatKhauCu;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.TextBox txtHoVaTen;
+        private System.Windows.Forms.Label lblHoVaTen;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.Label lblTenDangNhap;
+        private System.Windows.Forms.Label lblThayDoiMatKhau;
+        private System.Windows.Forms.ComboBox cmbQuyenTruyCap;
+    }
+}
